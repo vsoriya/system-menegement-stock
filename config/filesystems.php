eb3171fa1d2dad24c5571adb8ad094e9a2d65af6
soriya <vseyhaksoriya@gmail.com>
2026-08-03 02:15:56 +0700

    Surface S3 image upload failures

--- config/filesystems.php
+++ config/filesystems.php
@@ -88,7 +88,7 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'visibility' => env('AWS_VISIBILITY', 'public'),
-            'throw' => false,
+            'throw' => true,
             'report' => false,
         ],
 
